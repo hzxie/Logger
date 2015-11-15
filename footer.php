e1c3f70b46df5e14cea05cc4f8da6ea42cd5d844
Xie Haozhe <zjhzxhz@gmail.com>
2015-11-15 15:26:09 +0800

    Complete primary navigation in header.

--- footer.php
+++ footer.php
@@ -10,6 +10,38 @@
  */
 ?>
     <?php wp_footer(); ?>
+    <a id="back-to-top" href="javascript:void(0);">
+        <i class="fa fa-chevron-up"></i>
+    </a>
+    <!-- JavaScript -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript">
+        (function($) {
+            $('#primary-navigation li.menu-item-has-children').hover(function() {
+                $(this).addClass('hover');
+                $('> .sub-menu', this).slideDown(100);
+            }, function() {
+                $(this).removeClass('hover');
+                $('> .sub-menu', this).slideUp(100);
+            });
+        })(jQuery);
+    </script>
+    <script type="text/javascript">
+        (function($) {
+            $(window).scroll(function () {
+                if( $(this).scrollTop() > 100 ) {
+                    $("#back-to-top").css("right","20px");
+                } else {
+                    $("#back-to-top").css("right","-60px");
+                }
+            });
+            $("#back-to-top").click(function() {
+                $('html,body').animate({
+                    scrollTop: 0
+                }, 500);
+            });
+        })(jQuery);
+    </script>
     <!-- Analytics Code -->
     <?php if ( ot_get_option(TPLNAME . '_analytics_code') ): ?>
     <?php echo ot_get_option(TPLNAME . '_analytics_code'); ?>
